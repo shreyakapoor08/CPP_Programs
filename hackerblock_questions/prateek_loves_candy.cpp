dde9c0bbb7e82ccf330ef6b13e9d17ef41436806
Shreya Kapoor <shreyakapoor98@gmail.com>
2019-03-26 00:34:17 +0530

    prateek love candy

--- hackerblock_questions/prateek_loves_candy.cpp
+++ hackerblock_questions/prateek_loves_candy.cpp
@@ -35,8 +35,32 @@ int main(){
     cin>>t;
 
     int n;
-    for(int i=0;i<=t;i++){
+    for(int i=0;i<t;i++){
         cin>>n;
         cout<<primes[n-1]<<endl;
     }
 }
+
+/*
+PRATEEK LOVES CANDY
+Prateek recently graduated from college. To celebrate, he went to a candy shop and bought all the candies. The total cost was a number upto which there are n prime numbers (starting from 2). Since Prateek wants to minimize his cost, he calls you to help him find the minimum amount that needs to be paid. Being a student of Prateek bhaiya it is now your job to help him out :)
+
+Input Format:
+First line contains a single integer denoting the number of test cases T. Next T lines contains a single integer N, denoting the number of primes required.
+
+Constraints:
+T <= 10000
+
+It is guaranteed that the answer does not exceed 10^6.
+
+Output Format
+Print the minimum cost that needs to be paid.
+
+Sample Input
+2
+5
+1
+Sample Output
+11
+2
+*/

a24587c94c00d53d00dc9bfb75be9dfa640c4308
Shreya Kapoor <shreyakapoor98@gmail.com>
2019-03-26 00:08:00 +0530

    decimaltooctal start

---
+++ decimaltooctal.cpp
@@ -0,0 +1,14 @@
+/*
+DECIMAL TO OCTAL
+Take N (number in decimal format). Write a function that converts it to octal format. Print the value returned.
+
+Input Format:
+Constraints:
+0 < N <= 1000000000
+
+Output Format
+Sample Input
+63
+Sample Output
+77
+*/

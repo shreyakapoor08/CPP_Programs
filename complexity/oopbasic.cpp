754b3662530113eaaa9b2383bde601815248e83d
Shreya Kapoor <shreyakapoor98@gmail.com>
2019-07-09 19:24:15 +0530

    oo

--- complexity/oopbasic.cpp
+++ complexity/oopbasic.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 ///class is just a piece of code representing how a car bucket will look like
 class Car{
+    public:
     char name[20]; ///ek string jismai 100 character ho sakte hai
     //char[18]; allocation 4 4 ke size mai ho raha hai so ans is 24
     //char name[13]; 13 + 4 = 17 , 17 ka nearest multiple of 4 is 20
@@ -16,4 +17,14 @@ int main()
 {
     Car A; // A is obj of that class, and object take memory
     cout<<sizeof(A)<<endl; //20 bytes for name and 4 byte for price
+
+    A.price = 100;
+
+    A.name[0] = 'b';
+    A.name[1] = 'm';
+    A.name[2] = 'w';
+    A.name[3] = '\0';
+
+    cout<<A.name<<endl;;
+    cout<<A.price<<endl;
 }

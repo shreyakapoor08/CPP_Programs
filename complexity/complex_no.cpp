c20b805ec95a91fb47c4839a07ca5a2bcddc2499
Shreya Kapoor <shreyakapoor98@gmail.com>
2019-07-11 01:02:05 +0530

    com no

--- complexity/complex_no.cpp
+++ complexity/complex_no.cpp
@@ -52,18 +52,32 @@ public:
     }
 };
 
+ostream& operator<<(ostream&o1, ComplexNo&o2) //o1 is ref var to cout
+{
+    o2.print();
+    return o1;
+}
+/*istream& operator>>(stream&o1, ComplexNo&o2) //o1 is ref var to cout
+{
+    o2.print();
+    return o1;
+}
+*/
+
 int main()
 {
     ComplexNo c1(1,2);
-    c1.print();
+    //c1.print();
+
+    cout<<c1;
 
     ComplexNo c2;
     c2.input();
     c2.print();
 
     c1 + c2;
-    c1.print();
-
+    //c1.print();
+    cout<<c1<<c2;
     !c1; //conjugate
     c1.print();
 

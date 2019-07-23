9cb9a125c0b97c5bb2674da5677732e603552af7
Shreya Kapoor <shreyakapoor98@gmail.com>
2019-07-23 23:57:03 +0530

    closest number

--- sudo placement 2019/closestnumber.cpp
+++ sudo placement 2019/closestnumber.cpp
@@ -31,9 +31,75 @@ Output:
 #include<iostream>
 using namespace std;
 
+int getClosest(int val1,int val2, int k)
+{
+    if(k - val1 >= val2 - k)
+        return val2;
+    else
+        return val1;
+}
+
+//returns element closest to k in arr[]
+int findClosest(int arr[],int n,int k)
+{
+    //corner cases
+    if(k <= arr[0])
+        return arr[0];
+    if(k >= arr[n-1])
+        return arr[n-1];
+
+    //doing binary search
+    int i=0,j=n,mid=0;
+    while(i<j)
+    {
+        mid = (i+j)/2;
+
+        if(arr[mid] == k)
+            return arr[mid];
+
+        //if k is less than array element then search in left
+        if(k < arr[mid])
+        {
+            //if k is greater than previous to mid
+            //return closest of two
+            if(mid>0 && k > arr[mid - 1])
+                return getClosest(arr[mid - 1], arr[mid],k);
+
+            //repeat for left half
+            j = mid;
+        }
+
+        //if k is greater than mid
+        else{
+            if(mid < n-1 && k < arr[mid + 1])
+                return getClosest(arr[mid], arr[mid+1],k);
+
+            //update i
+            i = mid + 1;
+
+        }
+    }
+
+    //only single element left after search
+    return arr[mid];
+}
 int main()
 {
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int n,k;
+        cin>>n>>k;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        cout<<(findClosest(arr,n,k));
+        cout<<endl;
 
+    }
 
     return 0;
 }

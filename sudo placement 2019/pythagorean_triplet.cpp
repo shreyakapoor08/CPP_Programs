83acc80660415c773bf0c65ced864d351d5c5667
Shreya Kapoor <shreyakapoor98@gmail.com>
2019-07-23 22:15:19 +0530

    rotatr array

--- sudo placement 2019/pythagorean_triplet.cpp
+++ sudo placement 2019/pythagorean_triplet.cpp
@@ -26,11 +26,58 @@ Testcase 1: a=3, b=4, and c=5 forms a pythagorean triplet, so we print "Yes"
 */
 
 #include<iostream>
+#include<algorithm>
 using namespace std;
+
+//function that returns true if there is a triplet
+bool isTriplet(int arr[], int n)
+{
+   //square array elements
+   for(int i=0;i<n;i++)
+    arr[i] = arr[i] * arr[i];
+
+   //sort array element
+   sort(arr, arr+n);
+
+   //now fix one element one by one and find the other two elements
+   for(int i=n-1;i>=2;i--)
+   {
+       //to find the other two elements, start two index
+       //variables from two corners of the array and move
+       //them towards each other
+       int l = 0; //index of first element in arr[0...i-1]
+       int r = i-1; //index of last element in 0...i-1
+       while(l<r)
+       {
+           //a triplet found
+           if(arr[l] + arr[r] == arr[i])
+            return true;
+
+           //else either move 'l' or 'r'
+           (arr[l] + arr[r] < arr[i]) ? l++ : r--;
+       }
+   }
+
+   //if we reach here, then no triplet found
+   return false;
+}
 
 int main()
 {
-
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        isTriplet(arr,n) ? cout<< "Yes" : cout <<"No";
+        cout<<endl;
+    }
 
     return 0;
 }
---
+++ sudo placement 2019/rotatearray.cpp
@@ -0,0 +1,69 @@
+/*
+Given an unsorted array arr[] of size N, rotate it by D elements (clockwise).
+
+Input:
+The first line of the input contains T denoting the number of testcases. First line of eacg test case contains two space separated elements, N denoting the size of the array and an integer D denoting the number size of the rotation. Subsequent line will be the N space separated array elements.
+
+Output:
+For each testcase, in a new line, output the rotated array.
+
+Constraints:
+1 <= T <= 200
+1 <= N <= 107
+1 <= D <= N
+0 <= arr[i] <= 105
+
+Example:
+Input:
+2
+5 2
+1 2 3 4 5
+10 3
+2 4 6 8 10 12 14 16 18 20
+
+Output:
+3 4 5 1 2
+8 10 12 14 16 18 20 2 4 6
+
+Explanation :
+Testcase 1: 1 2 3 4 5  when rotated by 2 elements, it becomes 3 4 5 1 2.
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int n,d;
+        cin>>n>>d;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+
+        int temp[n];
+        for(int i=0;i<d;i++)
+        {
+            temp[i] = arr[i];
+        }
+        for(int i=d;i<n;i++)
+        {
+            arr[i-d] = arr[i];
+        }
+        for(int i=0;i<d;i++)
+        {
+            arr[n-d+i] = temp[i];
+        }
+        for(int i=0;i<n;i++)
+        {
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}

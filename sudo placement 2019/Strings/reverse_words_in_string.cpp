c7440b617d4e2fa3b238913e26e86e94c3d75539
Shreya Kapoor <shreyakapoor98@gmail.com>
2019-07-30 23:56:17 +0530

    validate ip

--- sudo placement 2019/Strings/reverse_words_in_string.cpp
+++ sudo placement 2019/Strings/reverse_words_in_string.cpp
@@ -1,1 +1,47 @@
+/*
+Given a String of length S, reverse the whole string without reversing the individual words in it. Words are separated by dots.
 
+Input:
+The first line contains T denoting the number of testcases. T testcases follow. Each case contains a string S containing characters.
+
+Output:
+For each test case, in a new line, output a single line containing the reversed String.
+
+Constraints:
+1 <= T <= 100
+1 <= |S| <= 2000
+
+Example:
+Input:
+2
+i.like.this.program.very.much
+pqr.mno
+
+Output:
+much.very.program.this.like.i
+mno.pqr
+*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        string s;
+        cin>>s;
+        int size = s.size();
+        for(int i=size-1;i>=0;i--)
+        {
+            if(s[i]=='.' || i==0)
+            {
+                int k;
+
+            }
+        }
+    }
+}

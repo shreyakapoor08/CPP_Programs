04f7080e7b09e83bf81d3699730e5fe5c8149681
shreyakapoor08 <shreyakapoor98@gmail.com>
2019-11-16 23:59:07 +0530

    jump cloud

---
+++ hackerrank/countingvalleys.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int n,updown=0,res=0;
+    char c;
+    cin>>n;
+
+    for(int i=0;i<n;i++)
+    {
+        cin>>c;
+        if(c=='U')
+            updown++;
+        else
+            updown--;
+        if(updown==0&&c=='U')
+            res++;
+    }
+    cout<<res<<endl;
+}
